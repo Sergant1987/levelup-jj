39dc23dc4e436b28b76ed3674feb301081d48a8a
Marchenko Sergei <63511370+Sergant1987@users.noreply.github.com>
2021-03-13 12:37:18 +0300

    refactor DBConfiguration

--- src/main/java/com/marchenko/medcards/config/DBConfiguration.java
+++ src/main/java/com/marchenko/medcards/config/DBConfiguration.java
@@ -1,36 +1,24 @@
 package com.marchenko.medcards.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.dao.annotation.PersistenceExceptionTranslationPostProcessor;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.JpaVendorAdapter;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
 import javax.sql.DataSource;
 import java.util.Properties;
 
 @Configuration
 @EnableTransactionManagement
-//@ComponentScan({"com.marchenko.medcards.service","com.marchenko.medcards.security"})
-//@EnableJpaRepositories(basePackages = "com.marchenko.medcards.repository")
 public class DBConfiguration {
-
-    //
-
 
     @Profile("dev")
     @Bean(name="entityManagerFactory")
@@ -41,7 +29,6 @@ public class DBConfiguration {
         JpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
         em.setJpaVendorAdapter(vendorAdapter);
         em.setJpaProperties(additionalPropertiesForTest());
-
         return em;
     }
 
@@ -65,13 +52,10 @@ public class DBConfiguration {
         JpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
         em.setJpaVendorAdapter(vendorAdapter);
         em.setJpaProperties(additionalProperties());
-
         return em;
     }
 
-
-
-//    @Profile("prod")
+    @Profile("prod")
     @Bean
     public DataSource dataSource() {
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
@@ -82,12 +66,10 @@ public class DBConfiguration {
         return dataSource;
     }
 
-
     @Bean
     public PlatformTransactionManager transactionManager(EntityManagerFactory emf) {
         JpaTransactionManager transactionManager = new JpaTransactionManager();
         transactionManager.setEntityManagerFactory(emf);
-
         return transactionManager;
     }
 
@@ -95,7 +77,6 @@ public class DBConfiguration {
     public PersistenceExceptionTranslationPostProcessor exceptionTranslation() {
         return new PersistenceExceptionTranslationPostProcessor();
     }
-
 
     private Properties additionalProperties() {
         Properties properties = new Properties();
@@ -106,17 +87,13 @@ public class DBConfiguration {
         return properties;
     }
 
-
     private Properties additionalPropertiesForTest() {
         Properties properties = new Properties();
         properties.setProperty("hibernate.dialect", "org.hibernate.dialect.H2Dialect");
         properties.setProperty("hibernate.show_sql", "true");
         properties.setProperty("hibernate.format_sql", "true");
         properties.setProperty("hibernate.hbm2ddl.auto", "create-drop");
-
         return properties;
     }
-
-
 
 }
